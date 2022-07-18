6206f74bc56c0acd207fbb6c1bc16ad1caa31bed
OceanSkys <90655578+OceanSkys@users.noreply.github.com>
2022-07-18 13:26:51 -0700

    Fixed carousel again

--- js/btn.js
+++ js/btn.js
@@ -1,42 +1,17 @@
-// const button = document.querySelector('.btn');
-
-// const displayButton = () => {
-//   window.addEventListener('scroll', () => {
-//     console.log(window.scrollY);
-  
-//     if (window.scrollY > 1100) {
-//       button.style.display = "block";
-//     } else {
-//       button.style.display = "none";
-//     }
-//   });
-// };
-
-// const scrollToTop = () => {
-//   button.addEventListener("click", () => {
-//     window.scroll({
-//       top: 0,
-//       left: 0,
-//       behavior: 'smooth'
-//     }); 
-//     console.log(event);
-//   });
-// };
-
-// displayButton();
-// scrollToTop();
-
-
-$(document).ready(function(){ 
-  $(window).scroll(function(){ 
-      if ($(this).scrollTop() > 100) { 
-          $('#scroll').fadeIn(); 
-      } else { 
-          $('#scroll').fadeOut(); 
-      } 
-  }); 
-  $('#scroll').click(function(){ 
-      $("html, body").animate({ scrollTop: 0 }, 600); 
-      return false; 
-  }); 
-});
+$(document).ready(function() {
+    // Show or hide the sticky footer button
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 200) {
+        $('.go-top').fadeIn(200);
+      } else {
+        $('.go-top').fadeOut(200);
+      }
+    });
+    
+    // Animate the scroll to top
+    $('.go-top').click(function(event) {
+      event.preventDefault();
+      
+      $('html, body').animate({scrollTop: 0}, 300);
+    })
+  });
